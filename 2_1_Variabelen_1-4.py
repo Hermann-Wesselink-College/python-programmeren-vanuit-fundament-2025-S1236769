6800213c78dd46226dc905d4d4f8e0f7f7b50685
S1236769 <36768@lln.hethwc.nl>
2025-09-15 09:33:18 +0000

    modified:   2_1_Variabelen_1-4.py

--- 2_1_Variabelen_1-4.py
+++ 2_1_Variabelen_1-4.py
@@ -1,1 +1,9 @@
 # opgaven 1 tot en met 4 van paragraaf 2.1 Variabelen
+print("Hello World")
+Verzendkosten = 5
+print(verzendkosten) 
+productcode = "A136i"                  
+print(productcode)
+price = 9.95
+print(price)
+
