edfaa9889d47a56e7509148447822d59f8992e09
S1236769 <36768@lln.hethwc.nl>
2025-09-18 07:42:04 +0000

    modified:   2_6_Variabelen_1-5.py

--- 2_6_Variabelen_1-5.py
+++ 2_6_Variabelen_1-5.py
@@ -1,1 +1,12 @@
 # opdracht 1 tpt en met 5 van paragraaf 2.6
+postcode = "1187BT"
+woonplaats = "amstelveen"
+print(woonplaats.upper(), postcode.lower())
+verzendmethode = "PostNL"
+betaalmethode = "iDEAL"
+print(verzendmethode.lower())
+print(betaalmethode.upper())
+postcode = input("wat is je postcode?")
+print(postcode())
+plaats = "Gasselternijveenschemond"
+print(len(plaats))
